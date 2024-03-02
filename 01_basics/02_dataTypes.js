a917113bf84b15e62550fb5dbdfa84c2c1eabd1d
AryaBardhan <aryabardhan2002@gmail.com>
2024-03-02 14:30:20 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("hello") // we are using node.js not browser
+
+
+//we dont write two console.log in same line because it reduces the code readability
+
+console.log(fafa); console.log(afa); //------XXXXXXXX
+
+
+let name = "Arya" //STRING
+let age = 18 // INTEGER 
+let isLoggedIn = false //BOOLEAN
+
+
+//primitive datatypes
+
+// NUMBER => 2^53
+// BIGINT
+// STRING => ""
+// BOOLEAN =>TRUE/FALSE
+// NULL => STANDALONE VALUE
+// UNDEFINED => 
+// SYMBOL => UNIQUE
+
+//OBJECT 
+
+
+//null is an object in javascript
+
+
+console.log(typeof "Arya"); // to identify the datatype of any data
